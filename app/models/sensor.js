70990a968e53f0a796d26475c0dcada6f6929ac6
bmax <bmax1337@gmail.com>
2014-08-08 12:13:17 -0400

    Better working version, includes simple-auth

--- app/models/sensor.js
+++ app/models/sensor.js
@@ -3,5 +3,5 @@ import DS from "ember-data";
 export default DS.Model.extend({
  	type: DS.attr('string'),
  	readings: DS.hasMany('reading'),
-  user: DS.belongsTo('user')
+  	user: DS.belongsTo('user')
 });
