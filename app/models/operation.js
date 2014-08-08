498c754ec57a7357b525b4acd2d977fc513e4c4e
Sean Rabaut <rabautse@gmail.com>
2014-08-08 17:30:47 -0400

    Configured RESTAdapter for localhost:3000

--- app/models/operation.js
+++ app/models/operation.js
@@ -1,24 +1,5 @@
 import DS from 'ember-data';
 
-var Operation = DS.Model.extend({
+export default DS.Model.extend({
   name: DS.attr('string')
 });
-
-Operation.reopenClass({
-    FIXTURES: [
-        {
-            id: 1,
-            name: "Complete Ember.js Tutorial",
-        },
-        {
-            id: 2,
-            name: "Checkout some more ember stuff",
-        },
-        {
-            id: 3,
-            name: "Solve world hunger (with Ember)",
-        }
-    ]
-})
-
-export default Operation
