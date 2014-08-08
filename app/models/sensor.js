acdebb3a2e4d0feaabc05e0583f7b28ca27b7fe2
bmax <bmax1337@gmail.com>
2014-08-08 12:40:29 -0400

    added serializer

--- app/models/sensor.js
+++ app/models/sensor.js
@@ -2,6 +2,5 @@ import DS from "ember-data";
 
 export default DS.Model.extend({
  	type: DS.attr('string'),
- 	readings: DS.hasMany('reading'),
-  	user: DS.belongsTo('user')
-});
+ 	readings: DS.hasMany('reading')
+ });
